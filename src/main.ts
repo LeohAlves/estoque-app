aebfcfbf3b312878f7fe9ed607cd5e1b3b4c1a87
Leonardo Alves <leonardoalves72@hotmail.com>
2023-04-11 22:25:38 -0300

    terminando Crud

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+
+import { provideHttpClient } from '@angular/common/http'
 
 if (environment.production) {
   enableProdMode();
@@ -16,5 +19,6 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     importProvidersFrom(IonicModule.forRoot({})),
     provideRouter(routes),
+    provideHttpClient()
   ],
 });
